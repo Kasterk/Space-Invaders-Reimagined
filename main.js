e7f743dc1210917222364e9b63dbadb2b39170c6
Kim Kastermans <kim.kastermans@hva.nl>
2026-04-11 21:35:08 +0200

    Added Style to HTML. Added canvas background image.

--- main.js
+++ main.js
@@ -1,38 +1,14 @@
-//Board
-let tileSize = 80;
-let rows = 13;
-let columns = 13;
+const canvas = document.getElementById('board');
+const ctx = canvas.getContext('2d');
 
-let board;
-let boardWidth = tileSize * columns;
-let boardHeight = tileSize * rows;
-let context;
+canvas.width = 800;
+canvas.height = 700;
 
-//Ship
-let shipWidth = tileSize;
-let shipHeight = tileSize;
-let shipX = tileSize * columns / 2 - tileSize / 2;
-let shipY = tileSize * rows - tileSize * 2;
+const background = new Image();
+background.src = "Images/Background.jpg";
 
-let ship = {
-    x : shipX,
-    y : shipY,
-    width : shipWidth,
-    height : shipHeight
+function draw() {
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 }
 
-let shipImage;
-
-window.onload = function() {
-    board = document.getElementById("board");
-    board.width = boardWidth;
-    board.height = boardHeight;
-    context = board.getContext("2d");
-
-    shipImage = new Image();
-    shipImage.src = "/Images/Space-Invaders-Ship.png";
-    shipImage.onload = function() {
-        context.drawImage(shipImage, ship.x, ship.y, ship.width, ship.height);
-    }
-}
-
+setInterval(draw, 1000 / 60);
